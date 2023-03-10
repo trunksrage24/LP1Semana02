c6b4c084c77be65348d118b7eb26044afc4b3216
Vitor <vitor.daniel@outlook.pt>
2023-03-10 20:45:38 +0000

    couldnt figure out ex 9

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question, response;
+            Console.WriteLine("insert question: ");
+            question = Console.ReadLine();
+            response = question switch
+                {
+                    "how are you" => "im fine, thank you",
+                    "whats your name?" => "grandpa",
+                    "What's your mission?"=> "Destroy mankind!",
+                    _ => "You got me, I'm not THAT smart!",
+                };
+            while (question = switch)
+            {
+                // Responder adequadamente
+                Console.WriteLine(response);
+            }
+
+        }
+    }
+}
