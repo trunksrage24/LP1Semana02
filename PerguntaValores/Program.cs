ca3d8e067ec755849f718e5d8361cf5c31a22fdb
Vitor <vitor.daniel@outlook.pt>
2023-03-10 11:57:35 +0000

    added surface calc

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace PerguntaValores
 {
@@ -6,14 +6,18 @@ namespace PerguntaValores
     {
         static void Main()
         {
-            //input values for high and radius and calc volume
+            //input values for high and radius
             Console.WriteLine("enter high value: ");
             double a = double.Parse(Console.ReadLine());
             Console.WriteLine("enter radius value: ");
             double r = double.Parse(Console.ReadLine());
             double pi = 3.1415926;
+            //calc volume
             double V = pi * Math.Pow(r,2) * a;
-             Console.WriteLine("your cylinder volume is: " + V);
+            Console.WriteLine("your cylinder volume is: " + V);
+            //calc surface
+            double S = 2 * pi * r * (r + a);
+            Console.WriteLine("your cylinder surface is: " + S);
         }
     }
 }
