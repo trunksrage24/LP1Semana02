3510f9a1bb4236d21fc8fb347aa667bf2148a51d
Vitor <vitor.daniel@outlook.pt>
2023-03-10 21:28:52 +0000

    couldnt figure out ex 11

---
+++ CrossSquare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrossSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dont know how to do this one.")
+        }
+    }
+}
