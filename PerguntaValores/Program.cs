7f76fc636b12e69112098aa6c73314912e7ecef3
Vitor <vitor.daniel@outlook.pt>
2023-03-10 16:27:06 +0000

    ex6 parte 1

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -11,7 +11,7 @@ namespace PerguntaValores
             double a = double.Parse(Console.ReadLine());
             Console.WriteLine("enter radius value: ");
             double r = double.Parse(Console.ReadLine());
-            double pi = Math.Pi;
+            double pi = Math.PI;
             //calc volume
             double V = pi * Math.Pow(r,2) * a;
             Console.WriteLine("your cylinder volume is: " + V);
---
+++ Specials/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main()
+        {
+            //double
+            Console.WriteLine("max value for double " + double.MaxValue);
+            Console.WriteLine("min value for double " + double.MinValue);
+            //float
+            Console.WriteLine("max value for decimal " + decimal.MaxValue);
+            Console.WriteLine("min value for decimal " + decimal.MinValue);
+            //decimal
+            Console.WriteLine("max value for float " + float.MaxValue);
+            Console.WriteLine("min value for float " + float.MinValue);
+            //short
+            Console.WriteLine("max value for short" + short.MaxValue);
+            Console.WriteLine("min value for short" + short.MinValue);
+            //ushort
+            Console.WriteLine("max value for ushort" + ushort.MaxValue);
+            Console.WriteLine("min value for ushort" + ushort.MinValue);
+            //sbyte
+            Console.WriteLine("max value for sbyte " + sbyte.MaxValue);
+            Console.WriteLine("min value for sbyte " + sbyte.MinValue);
+            //byte
+            Console.WriteLine("max value for byte " + byte.MaxValue);
+            Console.WriteLine("min value for byte " + byte.MinValue);
+            //ulong
+            Console.WriteLine("max value for ulong " + ulong.MaxValue);
+            Console.WriteLine("min value for ulong " + ulong.MinValue);
+            //long
+            Console.WriteLine("max value for long" + long.MaxValue);
+            Console.WriteLine("min value for long" + long.MinValue);
+            //int
+            Console.WriteLine("max value for int " + int.MaxValue);
+            Console.WriteLine("min value for int " + int.MinValue);
+            //uint
+            Console.WriteLine("max value for uint " + uint.MaxValue);
+            Console.WriteLine("min value for uint " + uint.MinValue);
+            //char
+            Console.WriteLine("max value for char " + char.MaxValue);
+            Console.WriteLine("min value for char " + char.MinValue);
+        }
+    }
+}
