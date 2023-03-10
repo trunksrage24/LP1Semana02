2b04cc2174d1b74757d5db20ab95a37adf15fe04
Vitor <vitor.daniel@outlook.pt>
2023-03-10 20:49:14 +0000

    dont know how to do ex 10

---
+++ Triangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("its suposed to take  int and char and print nr of lines as int with char chosen");
+        }
+    }
+}
