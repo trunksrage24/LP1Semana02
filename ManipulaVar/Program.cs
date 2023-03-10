eac6f2b7240996486a3b447dcc4691ca1717afb7
Vitor <vitor.daniel@outlook.pt>
2023-03-10 11:49:34 +0000

    fixeed repository misplacements

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input arg and covert to int
+            string argu = (args[0]);
+
+            int x = int.Parse(argu);
+            Console.WriteLine(argu);
+            Console.WriteLine(" increment '++' after var x: " + x++);
+            int y = --x;
+            Console.WriteLine(" decrement '--' before var x: " + y);
+
+        }
+    }
+}
+
---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main()
+        {
+            //input values for high and radius and calc volume
+            Console.WriteLine("enter high value: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter radius value: ");
+            double r = double.Parse(Console.ReadLine());
+            double pi = 3.1415926;
+            double V = pi * Math.Pow(r,2) * a;
+             Console.WriteLine("your cylinder volume is: " + V);
+        }
+    }
+}
