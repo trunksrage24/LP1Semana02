c53e8789c922c4281158f61318c591c6c36f3449
Vitor <vitor.daniel@outlook.pt>
2023-03-10 20:28:57 +0000

    ex8

---
+++ ChatGPTGrandfather/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ChatGPTGrandfather
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            switch (question)
+            {
+                case "How are you?":
+                    response = "I'm fine, thank you";
+                    break;
+                case "What's your name?":
+                    response = "Gran'pa";
+                    break;
+                case "What's your mission?":
+                    response = "Destroy mankind!";
+                    break;
+                default:
+                    response = "You got me, I'm not THAT smart!";
+                    break;
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}
---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+            response = question switch
+            {
+                "how are you" => "im fine, thank you",
+                "whats your name?" => "grandpa",
+                "What's your mission?"=> "Destroy mankind!",
+                _ => "You got me, I'm not THAT smart!",
+            };
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}
